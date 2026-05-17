20c8a879b80dfd8c555116bfbbfc5022abc2e402
GabrielMira2009 <gabrielmira2009@gmail.com>
2026-05-17 17:07:08 -0300

    :art: Jogo da velha Simples

---
+++ JogoDaVelha/JogoDaVelha/MainForm.Designer.cs
@@ -0,0 +1,340 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Aluno
+ * Date: 30/09/2025
+ * Time: 14:48
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace JogoDaVelha
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Slot1 = new System.Windows.Forms.Panel();
+			this.Marcacao1 = new System.Windows.Forms.Label();
+			this.Slot2 = new System.Windows.Forms.Panel();
+			this.Marcacao2 = new System.Windows.Forms.Label();
+			this.Slot3 = new System.Windows.Forms.Panel();
+			this.Marcacao3 = new System.Windows.Forms.Label();
+			this.Slot4 = new System.Windows.Forms.Panel();
+			this.Marcacao4 = new System.Windows.Forms.Label();
+			this.Slot6 = new System.Windows.Forms.Panel();
+			this.Marcacao5 = new System.Windows.Forms.Label();
+			this.Slot7 = new System.Windows.Forms.Panel();
+			this.Marcacao6 = new System.Windows.Forms.Label();
+			this.Slot8 = new System.Windows.Forms.Panel();
+			this.Marcacao7 = new System.Windows.Forms.Label();
+			this.Slot9 = new System.Windows.Forms.Panel();
+			this.Marcacao8 = new System.Windows.Forms.Label();
+			this.Slot10 = new System.Windows.Forms.Panel();
+			this.Marcacao9 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.Slot1.SuspendLayout();
+			this.Slot2.SuspendLayout();
+			this.Slot3.SuspendLayout();
+			this.Slot4.SuspendLayout();
+			this.Slot6.SuspendLayout();
+			this.Slot7.SuspendLayout();
+			this.Slot8.SuspendLayout();
+			this.Slot9.SuspendLayout();
+			this.Slot10.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// Slot1
+			// 
+			this.Slot1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Slot1.Controls.Add(this.Marcacao1);
+			this.Slot1.Location = new System.Drawing.Point(154, 46);
+			this.Slot1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Slot1.Name = "Slot1";
+			this.Slot1.Size = new System.Drawing.Size(58, 56);
+			this.Slot1.TabIndex = 0;
+			// 
+			// Marcacao1
+			// 
+			this.Marcacao1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Marcacao1.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.Marcacao1.Location = new System.Drawing.Point(-2, 0);
+			this.Marcacao1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.Marcacao1.Name = "Marcacao1";
+			this.Marcacao1.Size = new System.Drawing.Size(58, 54);
+			this.Marcacao1.TabIndex = 0;
+			this.Marcacao1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.Marcacao1.Click += new System.EventHandler(this.Marcacao1Click);
+			this.Marcacao1.DoubleClick += new System.EventHandler(this.Marcacao1DoubleClick);
+			// 
+			// Slot2
+			// 
+			this.Slot2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Slot2.Controls.Add(this.Marcacao2);
+			this.Slot2.Location = new System.Drawing.Point(216, 46);
+			this.Slot2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Slot2.Name = "Slot2";
+			this.Slot2.Size = new System.Drawing.Size(58, 56);
+			this.Slot2.TabIndex = 1;
+			// 
+			// Marcacao2
+			// 
+			this.Marcacao2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Marcacao2.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.Marcacao2.Location = new System.Drawing.Point(-2, -1);
+			this.Marcacao2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.Marcacao2.Name = "Marcacao2";
+			this.Marcacao2.Size = new System.Drawing.Size(58, 54);
+			this.Marcacao2.TabIndex = 8;
+			this.Marcacao2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.Marcacao2.Click += new System.EventHandler(this.Marcacao2Click);
+			this.Marcacao2.DoubleClick += new System.EventHandler(this.Marcacao2DoubleClick);
+			// 
+			// Slot3
+			// 
+			this.Slot3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Slot3.Controls.Add(this.Marcacao3);
+			this.Slot3.Location = new System.Drawing.Point(278, 46);
+			this.Slot3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Slot3.Name = "Slot3";
+			this.Slot3.Size = new System.Drawing.Size(58, 56);
+			this.Slot3.TabIndex = 2;
+			// 
+			// Marcacao3
+			// 
+			this.Marcacao3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Marcacao3.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.Marcacao3.Location = new System.Drawing.Point(-2, -1);
+			this.Marcacao3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.Marcacao3.Name = "Marcacao3";
+			this.Marcacao3.Size = new System.Drawing.Size(58, 54);
+			this.Marcacao3.TabIndex = 1;
+			this.Marcacao3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.Marcacao3.Click += new System.EventHandler(this.Marcacao3Click);
+			this.Marcacao3.DoubleClick += new System.EventHandler(this.Marcacao3DoubleClick);
+			// 
+			// Slot4
+			// 
+			this.Slot4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Slot4.Controls.Add(this.Marcacao4);
+			this.Slot4.Location = new System.Drawing.Point(154, 105);
+			this.Slot4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Slot4.Name = "Slot4";
+			this.Slot4.Size = new System.Drawing.Size(58, 56);
+			this.Slot4.TabIndex = 3;
+			// 
+			// Marcacao4
+			// 
+			this.Marcacao4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Marcacao4.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.Marcacao4.Location = new System.Drawing.Point(-4, 0);
+			this.Marcacao4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.Marcacao4.Name = "Marcacao4";
+			this.Marcacao4.Size = new System.Drawing.Size(58, 54);
+			this.Marcacao4.TabIndex = 4;
+			this.Marcacao4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.Marcacao4.Click += new System.EventHandler(this.Marcacao4Click);
+			this.Marcacao4.DoubleClick += new System.EventHandler(this.Marcacao4DoubleClick);
+			// 
+			// Slot6
+			// 
+			this.Slot6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Slot6.Controls.Add(this.Marcacao5);
+			this.Slot6.Location = new System.Drawing.Point(216, 105);
+			this.Slot6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Slot6.Name = "Slot6";
+			this.Slot6.Size = new System.Drawing.Size(58, 56);
+			this.Slot6.TabIndex = 4;
+			// 
+			// Marcacao5
+			// 
+			this.Marcacao5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Marcacao5.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.Marcacao5.Location = new System.Drawing.Point(-1, 0);
+			this.Marcacao5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.Marcacao5.Name = "Marcacao5";
+			this.Marcacao5.Size = new System.Drawing.Size(58, 54);
+			this.Marcacao5.TabIndex = 3;
+			this.Marcacao5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.Marcacao5.Click += new System.EventHandler(this.Marcacao5Click);
+			this.Marcacao5.DoubleClick += new System.EventHandler(this.Marcacao5DoubleClick);
+			// 
+			// Slot7
+			// 
+			this.Slot7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Slot7.Controls.Add(this.Marcacao6);
+			this.Slot7.Location = new System.Drawing.Point(278, 105);
+			this.Slot7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Slot7.Name = "Slot7";
+			this.Slot7.Size = new System.Drawing.Size(58, 56);
+			this.Slot7.TabIndex = 2;
+			// 
+			// Marcacao6
+			// 
+			this.Marcacao6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Marcacao6.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.Marcacao6.Location = new System.Drawing.Point(0, 0);
+			this.Marcacao6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.Marcacao6.Name = "Marcacao6";
+			this.Marcacao6.Size = new System.Drawing.Size(58, 54);
+			this.Marcacao6.TabIndex = 2;
+			this.Marcacao6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.Marcacao6.Click += new System.EventHandler(this.Marcacao6Click);
+			this.Marcacao6.DoubleClick += new System.EventHandler(this.Marcacao6DoubleClick);
+			// 
+			// Slot8
+			// 
+			this.Slot8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Slot8.Controls.Add(this.Marcacao7);
+			this.Slot8.Location = new System.Drawing.Point(154, 164);
+			this.Slot8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Slot8.Name = "Slot8";
+			this.Slot8.Size = new System.Drawing.Size(58, 56);
+			this.Slot8.TabIndex = 5;
+			// 
+			// Marcacao7
+			// 
+			this.Marcacao7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Marcacao7.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.Marcacao7.Location = new System.Drawing.Point(-2, 0);
+			this.Marcacao7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.Marcacao7.Name = "Marcacao7";
+			this.Marcacao7.Size = new System.Drawing.Size(58, 54);
+			this.Marcacao7.TabIndex = 5;
+			this.Marcacao7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.Marcacao7.Click += new System.EventHandler(this.Marcacao7Click);
+			this.Marcacao7.DoubleClick += new System.EventHandler(this.Marcacao7DoubleClick);
+			// 
+			// Slot9
+			// 
+			this.Slot9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Slot9.Controls.Add(this.Marcacao8);
+			this.Slot9.Location = new System.Drawing.Point(216, 164);
+			this.Slot9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Slot9.Name = "Slot9";
+			this.Slot9.Size = new System.Drawing.Size(58, 56);
+			this.Slot9.TabIndex = 6;
+			// 
+			// Marcacao8
+			// 
+			this.Marcacao8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Marcacao8.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.Marcacao8.Location = new System.Drawing.Point(-2, 0);
+			this.Marcacao8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.Marcacao8.Name = "Marcacao8";
+			this.Marcacao8.Size = new System.Drawing.Size(58, 54);
+			this.Marcacao8.TabIndex = 6;
+			this.Marcacao8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.Marcacao8.Click += new System.EventHandler(this.Marcacao8Click);
+			this.Marcacao8.DoubleClick += new System.EventHandler(this.Marcacao8DoubleClick);
+			// 
+			// Slot10
+			// 
+			this.Slot10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.Slot10.Controls.Add(this.Marcacao9);
+			this.Slot10.Location = new System.Drawing.Point(278, 164);
+			this.Slot10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Slot10.Name = "Slot10";
+			this.Slot10.Size = new System.Drawing.Size(58, 56);
+			this.Slot10.TabIndex = 7;
+			// 
+			// Marcacao9
+			// 
+			this.Marcacao9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Marcacao9.ForeColor = System.Drawing.SystemColors.HotTrack;
+			this.Marcacao9.Location = new System.Drawing.Point(0, 0);
+			this.Marcacao9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.Marcacao9.Name = "Marcacao9";
+			this.Marcacao9.Size = new System.Drawing.Size(58, 54);
+			this.Marcacao9.TabIndex = 7;
+			this.Marcacao9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.Marcacao9.Click += new System.EventHandler(this.Marcacao9Click);
+			this.Marcacao9.DoubleClick += new System.EventHandler(this.Marcacao9DoubleClick);
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.MediumAquamarine;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(152, 276);
+			this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(183, 49);
+			this.button1.TabIndex = 8;
+			this.button1.Text = "Novo jogo";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Honeydew;
+			this.ClientSize = new System.Drawing.Size(517, 405);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.Slot10);
+			this.Controls.Add(this.Slot9);
+			this.Controls.Add(this.Slot8);
+			this.Controls.Add(this.Slot7);
+			this.Controls.Add(this.Slot6);
+			this.Controls.Add(this.Slot4);
+			this.Controls.Add(this.Slot3);
+			this.Controls.Add(this.Slot2);
+			this.Controls.Add(this.Slot1);
+			this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "JogoDaVelha";
+			this.Slot1.ResumeLayout(false);
+			this.Slot2.ResumeLayout(false);
+			this.Slot3.ResumeLayout(false);
+			this.Slot4.ResumeLayout(false);
+			this.Slot6.ResumeLayout(false);
+			this.Slot7.ResumeLayout(false);
+			this.Slot8.ResumeLayout(false);
+			this.Slot9.ResumeLayout(false);
+			this.Slot10.ResumeLayout(false);
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label Marcacao9;
+		private System.Windows.Forms.Label Marcacao8;
+		private System.Windows.Forms.Label Marcacao7;
+		private System.Windows.Forms.Label Marcacao6;
+		private System.Windows.Forms.Label Marcacao5;
+		private System.Windows.Forms.Label Marcacao4;
+		private System.Windows.Forms.Label Marcacao3;
+		private System.Windows.Forms.Panel Slot10;
+		private System.Windows.Forms.Panel Slot9;
+		private System.Windows.Forms.Panel Slot8;
+		private System.Windows.Forms.Panel Slot7;
+		private System.Windows.Forms.Panel Slot6;
+		private System.Windows.Forms.Panel Slot4;
+		private System.Windows.Forms.Panel Slot3;
+		private System.Windows.Forms.Label Marcacao2;
+		private System.Windows.Forms.Panel Slot2;
+		private System.Windows.Forms.Label Marcacao1;
+		private System.Windows.Forms.Panel Slot1;
+	}
+}
---
+++ JogoDaVelha/JogoDaVelha/MainForm.cs
@@ -0,0 +1,167 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Aluno
+ * Date: 30/09/2025
+ * Time: 14:16
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace JogoDaVelha
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		
+
+		
+		void Marcacao2Click(object sender, EventArgs e)
+		{
+			Marcacao2.Text = "x";
+			this.Marcacao2.ForeColor = Color.Blue;
+			Marcacao2.Visible = true;
+		}
+		
+		void Marcacao2DoubleClick(object sender, EventArgs e)
+		{
+			Marcacao2.Text = "O";
+			this.Marcacao2.ForeColor = Color.Red;
+			Marcacao2.Visible = true;
+		}
+		
+		void Marcacao3Click(object sender, EventArgs e)
+		{
+			Marcacao3.Text = "x";
+			this.Marcacao3.ForeColor = Color.Blue;
+			Marcacao3.Visible = true;
+		}
+		
+		void Marcacao3DoubleClick(object sender, EventArgs e)
+		{
+			Marcacao3.Text = "O";
+			this.Marcacao3.ForeColor = Color.Red;
+			Marcacao3.Visible = true;
+		}
+		
+		void Marcacao4Click(object sender, EventArgs e)
+		{
+			Marcacao4.Text = "x";
+			this.Marcacao4.ForeColor = Color.Blue;
+			Marcacao4.Visible = true;
+		}
+		
+		void Marcacao4DoubleClick(object sender, EventArgs e)
+		{
+			Marcacao4.Text = "O";
+			this.Marcacao4.ForeColor = Color.Red;
+			Marcacao4.Visible = true;
+		}
+		
+		void Marcacao5Click(object sender, EventArgs e)
+		{
+			Marcacao5.Text = "x";
+			this.Marcacao5.ForeColor = Color.Blue;
+			Marcacao5.Visible = true;
+		}
+		
+		void Marcacao5DoubleClick(object sender, EventArgs e)
+		{
+			Marcacao5.Text = "O";
+			this.Marcacao5.ForeColor = Color.Red;
+			Marcacao5.Visible = true;
+		}
+		
+		void Marcacao6Click(object sender, EventArgs e)
+		{
+			Marcacao6.Text = "x";
+			this.Marcacao6.ForeColor = Color.Blue;
+			Marcacao6.Visible = true;
+		}
+		
+		void Marcacao6DoubleClick(object sender, EventArgs e)
+		{
+			Marcacao6.Text = "O";
+			this.Marcacao6.ForeColor = Color.Red;
+			Marcacao6.Visible = true;
+		}
+		
+		void Marcacao7Click(object sender, EventArgs e)
+		{
+			Marcacao7.Text = "x";
+			this.Marcacao7.ForeColor = Color.Blue;
+			Marcacao7.Visible = true;
+		}
+		
+		void Marcacao7DoubleClick(object sender, EventArgs e)
+		{
+			Marcacao7.Text = "O";
+			this.Marcacao7.ForeColor = Color.Red;
+			Marcacao7.Visible = true;
+		}
+		
+		void Marcacao8Click(object sender, EventArgs e)
+		{
+			Marcacao8.Text = "x";
+			this.Marcacao8.ForeColor = Color.Blue;
+			Marcacao8.Visible = true;
+		}
+		
+		void Marcacao8DoubleClick(object sender, EventArgs e)
+		{
+			Marcacao8.Text = "O";
+			this.Marcacao8.ForeColor = Color.Red;
+			Marcacao8.Visible = true;
+		}
+		
+		void Marcacao9Click(object sender, EventArgs e)
+		{
+			Marcacao9.Text = "x";
+			this.Marcacao9.ForeColor = Color.Blue;
+			Marcacao9.Visible = true;
+		}
+		
+		void Marcacao9DoubleClick(object sender, EventArgs e)
+		{
+			Marcacao9.Text = "O";
+			this.Marcacao9.ForeColor = Color.Red;
+			Marcacao9.Visible = true;
+		}
+		
+		void Marcacao1Click(object sender, EventArgs e)
+		{
+			Marcacao1.Text = "x";
+			this.Marcacao1.ForeColor = Color.Blue;
+			Marcacao1.Visible = true;
+		}
+		
+		void Marcacao1DoubleClick(object sender, EventArgs e)
+		{
+			Marcacao1.Text = "O";
+			this.Marcacao1.ForeColor = Color.Red;
+			Marcacao1.Visible = true;
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			Application.Restart();
+		}
+	}	
+}
