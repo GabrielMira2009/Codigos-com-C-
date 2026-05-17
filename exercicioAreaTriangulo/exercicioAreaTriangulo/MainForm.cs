bae7d7258b723ed426f31f4fa8e4281a7c22ddc4
GabrielMira2009 <gabrielmira2009@gmail.com>
2026-05-17 17:13:56 -0300

    :tada: Cálculo para área do triângulo

---
+++ exercicioAreaTriangulo/exercicioAreaTriangulo/MainForm.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace exercicioAreaTriangulo
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+		
+		}
+		
+	
+		
+	
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			int altura = int.Parse(textBox1.Text);
+			int bas = int.Parse(textBox2.Text);
+			
+			int vezes = altura * bas ;
+			int area = vezes/2;
+			
+			textBox3.Text = area.ToString();
+		}
+	}
+}
